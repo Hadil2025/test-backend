33002d522035089353c9399af9c044e5d5a3baf5
hadil <hadil.ab21@gmail.com>
2025-01-27 19:31:39 +0100

    adding my part

---
+++ routes/deliveryRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const {createDelivery,getAllDeliveries,updateDelivery,deleteDelivery,getDeliveryById,
+getDeliveriesByAddress,getDeliveriesByDate,getDeliveriesByStatus,getDeliveriesByOrderId,generateDeliveryPDF} = require('../controllers/deliveryController')
+const router = express.Router()
+
+router.post('/creat', createDelivery)
+router.get('/get', getAllDeliveries)
+router.get('/get/:id', getDeliveryById)
+router.get('/order', getDeliveriesByOrderId)
+router.put('/update/:id', updateDelivery)
+router.delete('/delete/:id', deleteDelivery)
+router.get('/findA', getDeliveriesByAddress)
+router.get('/findD', getDeliveriesByDate)
+router.get('/findS', getDeliveriesByStatus)
+router.get('/:id/pdf', generateDeliveryPDF)
+
+module.exports = router
